a57dd1f04e0bb85f45da96f0168335b40862aecd
Rowan Cardow <rowan@cardow.co>
2025-05-18 15:21:54 +1000

    update authToken and use .env instead of .env.local

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -48,7 +48,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1747544913143,
+    __BUILD_TIMESTAMP_MS__: 1747545622407,
     __NEXT_BASE_PATH__: "/home"
   });
 }
